47279ed3c8448b11273b5dbbf61a54ca8b4e4dde
alex960714 <alex_9607@mail.ru>
2017-10-30 02:22:01 +0300

    Deleted comments and .class files

--- src/com/appgui/appgui.java
+++ src/com/appgui/appgui.java
@@ -2,17 +2,12 @@ package com.appgui;
 
 import javax.imageio.ImageIO;
 import javax.swing.*;
-import javax.swing.table.AbstractTableModel;
-import javax.swing.table.TableModel;
 import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.*;
 
 public class appgui extends JPanel {
-    /*private JPanel mainField;
-    private JButton readyButton;
-    private JLabel statusLabel;*/
-    private Image image;
+    private BufferedImage image;
 
     public appgui() {
         try {
@@ -22,29 +17,11 @@ public class appgui extends JPanel {
             JOptionPane.showMessageDialog(null,message,"FATAL ERROR",JOptionPane.PLAIN_MESSAGE);
             System.exit(-1);
         }
-
-        /*readyButton=new JButton("Ready");
-        statusLabel=new JLabel("Status");*/
-        /*readyButton.setHorizontalAlignment(SwingConstants.LEFT);
-        readyButton.setVerticalAlignment(SwingConstants.BOTTOM);*/
-        /*readyButton.setAlignmentX(10.5f);
-        readyButton.setAlignmentY(10.5f);
-        this.setBounds(100,100,618,670);
-        Container container = this.getContentPane();
-        container.setLayout(new GridLayout(20,19,3,3));
-        readyButton.addActionListener(new ButtonClickListener());
-        container.add(readyButton);
-        container.add(statusLabel);
-        for(int i=0;i<19*19;i++){
-            container.add(new JLabel(""));
-        }*/
     }
 
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
-        //g = (Graphics2D) g;
-        //image = new ImageIcon("C:\\Users\\User\\IntelliJIDEAProjects\\LabConnect6\\src\\resources\\field.png").getImage();
         g.drawImage(image, 0, 0, this);
     }
 
@@ -54,7 +31,5 @@ public class appgui extends JPanel {
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(617, 630);
         frame.setVisible(true);
-        /*appgui app = new appgui();
-        app.setVisible(true);*/
     }
 }
